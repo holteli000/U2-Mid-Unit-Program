e2b47f3dda02e2e7bdad7d60006a630b661609e8
holteli000 <holteli000@warren.k12.in.us>
2021-09-20 13:18:01 -0400

    u2 mid project

--- src/SalesCommission.java
+++ src/SalesCommission.java
@@ -18,10 +18,58 @@ Total Earnings: $1184.38
 
  */
 
+/* put together a plan
+1. get inputs
+2. calculate
+3.
+*/
+
+
+import javax.swing.*;
+import java.text.DecimalFormat;
+
 public class SalesCommission {
 
     public static void main(String[] args) {
 
+        double hourlyRate = getInput("What is the hourly rate? ");
+        double hoursWorked = getInput("What is the hours worked? ");
+        double salesCommission = getInput("What is the sales commission? ");
+        double totalSales = getInput("What is the total sales? ");
+
+        double hourlyPay = hourlyPay(hourlyRate, hoursWorked);
+        double commission = commission(totalSales,salesCommission);
+        double totalPay = totalPay(hourlyPay, commission);
+
+        output(totalPay);
+
     }
 
+    public static double getInput(String prompt){
+
+        return Double.parseDouble(JOptionPane.showInputDialog(prompt));
+
+    }
+
+    public static double hourlyPay(double hourlyRate, double hoursWorked){
+        return hourlyRate * hoursWorked;
+
+
+    }
+
+    public static double commission(double totalSales, double salesCommission){
+      return  totalSales * (salesCommission/100.00);
+
+    }
+
+    public static double totalPay(double hourlyPay, double commission){
+        return hourlyPay + commission;
+
+    }
+    private static void output(double totalPay) {
+
+        DecimalFormat round = new DecimalFormat("#,###.00");
+        JOptionPane.showMessageDialog(null, " the total pay is " + round.format(totalPay));
+
+    }
 }
